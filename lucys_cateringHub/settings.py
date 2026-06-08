b2f0b1b79f9b34e5fbfd6071574c90ae15fd64d0
Chinenye125 <osonduchinenye125@gmail.com>
2026-06-08 09:17:23 +0200

    still in progress

--- lucys_cateringHub/settings.py
+++ lucys_cateringHub/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@55x+y4)7wh%1#9h5f!wp9oh!e@qtg$&+_%no7z$l&j5)^%)26'
+# SECRET_KEY = 'django-insecure-@55x+y4)7wh%1#9h5f!wp9oh!e@qtg$&+_%no7z$l&j5)^%)26'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
